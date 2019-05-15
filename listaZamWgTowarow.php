350f8235b9ee84a3a40986275a9d8b09f7a4ed1d
Ireneusz Stalicki <istalicki@gmail.com>
2019-05-15 15:52:22 +0200

    zmieniony warunek w zapytaniu przy widoku listaZamWgTowarów.

--- listaZamWgTowarow.php
+++ listaZamWgTowarow.php
@@ -2,7 +2,11 @@
 
 	require 'core/init.php';
 
-	$zamowieniaAll = getZamowienia($link, "t.id = {$_GET['id']}"); 
+	$sql = "zm.IdZamowienia in (select zm.IdZamowienia from zamowienia zm 
+								inner join zamowieniatow zt on zm.IdZamowienia = zt.IdZam 
+								inner join towary t on zt.Towar = t.id 
+								where t.id = {$_GET['id']} )";
+	$zamowieniaAll = getZamowienia($link, $sql); 
 	$towar = getTowar($link, $_GET['id']);
 	if (!$zamowieniaAll) {
 		$errorMessage = "Brak zamówień dla towaru <b>{$towar['nazwa']}</b>";
