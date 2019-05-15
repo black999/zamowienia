395a86c7078a8333e2d42a4e2131e601ee1f74ff
Ireneusz Stalicki <istalicki@gmail.com>
2019-05-15 16:20:06 +0200

    test

--- listaZamWgTowarow.php
+++ listaZamWgTowarow.php
@@ -7,11 +7,11 @@
 								inner join towary t on zt.Towar = t.id 
 								where t.id = {$_GET['id']} )";
 	$zamowieniaAll = getZamowienia($link, $sql); 
-	$towar = getTowar($link, $_GET['id']);
 	if (!$zamowieniaAll) {
 		$errorMessage = "Brak zamówień dla towaru <b>{$towar['nazwa']}</b>";
 	}
 
+	$towar = getTowar($link, $_GET['id']);
 	$tytul = "Lista zamówień na których znajduje się <b>{$towar['nazwa']}</b>";
 	$widok = 'okno';
 
