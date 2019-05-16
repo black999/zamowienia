db356688ada5b9ede4f839f17dd13528cb20b454
Ireneusz Stalicki <istalicki@gmail.com>
2019-05-16 07:58:39 +0200

    poprawki kosmetyczne

--- listaZamWgTowarow.php
+++ listaZamWgTowarow.php
@@ -6,13 +6,15 @@
 								inner join zamowieniatow zt on zm.IdZamowienia = zt.IdZam 
 								inner join towary t on zt.Towar = t.id 
 								where t.id = {$_GET['id']} )";
+
 	$zamowieniaAll = getZamowienia($link, $sql); 
+	$towar = getTowar($link, $_GET['id']);
+
 	if (!$zamowieniaAll) {
 		$errorMessage = "Brak zamówień dla towaru <b>{$towar['nazwa']}</b>";
 	}
 
-	$towar = getTowar($link, $_GET['id']);
-	$tytul = "Lista zamówień na których znajduje się <b>{$towar['nazwa']}</b>";
+	$tytul = "Lista zamówień na których znajduje/ą się <b>{$towar['nazwa']}</b>";
 	$widok = 'okno';
 
 	include ('view/listazamowien.view.php');	
